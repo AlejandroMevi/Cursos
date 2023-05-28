f4ba4d0b431e190f4d02823fbf6ae3336b8d59dd
AlejandroMevi <106292726+AlejandroMevi@users.noreply.github.com>
2023-05-28 10:06:44 -0600

    Tercer modulo
    Inicio del tercer modulo

---
+++ app/src/main/java/com/example/cursos/udemyCursos/Person.kt
@@ -0,0 +1,9 @@
+package com.example.cursos.udemyCursos
+
+class Person {
+    var alive : Boolean = true
+
+    fun die(){
+        alive = false
+    }
+}
---
+++ app/src/main/java/com/example/cursos/udemyCursos/PooActivity.kt
@@ -0,0 +1,21 @@
+package com.example.cursos.udemyCursos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cursos.R
+import com.example.cursos.databinding.ActivityPooBinding
+
+private lateinit var binding: ActivityPooBinding
+class PooActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPooBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var num : Int = 0
+        val jota : Person = Person()
+        println(jota.alive)
+        jota.die()
+        println(jota.alive)
+    }
+}
