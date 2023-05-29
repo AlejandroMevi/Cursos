32c64af073de2f0605548c04ffbc5841c73b1898
AlejandroMevi <106292726+AlejandroMevi@users.noreply.github.com>
2023-05-28 18:59:21 -0600

    Tercer modulo
    Código completo del tercer modulo

--- app/src/main/java/com/example/cursos/udemyCursos/Person.kt
+++ app/src/main/java/com/example/cursos/udemyCursos/Person.kt
@@ -1,9 +1,17 @@
 package com.example.cursos.udemyCursos
 
-class Person {
+import android.annotation.SuppressLint
+
+open class Person(var nombre : String = "Persona", var passport : String? = null){
     var alive : Boolean = true
 
+    @SuppressLint("NotConstructor")
+    fun Person(){
+        nombre = "Juan"
+        passport = "ASDA"
+    }
     fun die(){
         alive = false
     }
 }
+class Athlete(name: String, passport: String?, var sport: String): Person(name, passport)
