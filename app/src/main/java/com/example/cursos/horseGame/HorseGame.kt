7fd7629274faac72b139f7c0c22f1967cd723a46
AlejandroMevi <106292726+AlejandroMevi@users.noreply.github.com>
2023-07-25 17:56:44 -0600

    gameHorse
    gameHorse

---
+++ app/src/main/java/com/example/cursos/horseGame/HorseGame.kt
@@ -0,0 +1,12 @@
+package com.example.cursos.horseGame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cursos.R
+
+class HorseGame : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_horse_game)
+    }
+}
