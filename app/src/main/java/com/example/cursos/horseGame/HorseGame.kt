73c9ab5a9f612bb90249c1f6fb62e7dd7c05a57b
Alejandro Mejia <amejia@venturessoft.com>
2023-07-31 17:47:02 -0600

    GameHorse

--- app/src/main/java/com/example/cursos/horseGame/HorseGame.kt
+++ app/src/main/java/com/example/cursos/horseGame/HorseGame.kt
@@ -2,11 +2,13 @@ package com.example.cursos.horseGame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.cursos.R
+import com.example.cursos.databinding.ActivityHorseGameBinding
 
 class HorseGame : AppCompatActivity() {
+    private lateinit var binding: ActivityHorseGameBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_horse_game)
+        binding = ActivityHorseGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
