95769743f64f33e97ee1a619987ee032dfdf7cd2
AlejandroMevi <106292726+AlejandroMevi@users.noreply.github.com>
2023-08-11 16:11:58 -0600

    gamehorse
    gamehorse

--- app/src/main/java/com/example/cursos/horseGame/HorseGame.kt
+++ app/src/main/java/com/example/cursos/horseGame/HorseGame.kt
@@ -15,14 +15,48 @@ class HorseGame : AppCompatActivity() {
     private lateinit var binding: ActivityHorseGameBinding
     private var cellSelectedX = 0
     private var cellSelectedY = 0
+    private var options = 0
+    private var nameColorBlack = "black_cell"
+    private var nameColorWhite = "white_cell"
+
     private lateinit var board: Array<IntArray>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHorseGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        iniScreenGame()
-        setFirstPosition()
+
+        initScreenGame()
         resetBoard()
+        setFirstPosition()
+    }
+
+    fun checkCellClicked(v: View) {
+        val name = v.tag.toString()
+        val x = name.subSequence(1, 2).toString().toInt()
+        val y = name.subSequence(2, 3).toString().toInt()
+
+        checkCell(x, y)
+    }
+
+    private fun checkCell(x: Int, y: Int) {
+        val difX = x - cellSelectedX
+        val difY = y - cellSelectedY
+
+        var checkTrue = false
+
+        if (difX == 1 && difY == 2) checkTrue = true
+        if (difX == 1 && difY == -2) checkTrue = true
+        if (difX == 2 && difY == 1) checkTrue = true
+        if (difX == 2 && difY == -1) checkTrue = true
+        if (difX == -1 && difY == 2) checkTrue = true
+        if (difX == -1 && difY == -2) checkTrue = true
+        if (difX == -2 && difY == 1) checkTrue = true
+        if (difX == -2 && difY == -1) checkTrue = true
+
+        if (board[x][y] == 1) checkTrue = false
+
+        if (checkTrue) selectCell(x, y)
     }
 
     private fun resetBoard() {
@@ -62,8 +96,100 @@ class HorseGame : AppCompatActivity() {
 
         cellSelectedX = x
         cellSelectedY = y
-        
+
+        clearOptions()
+
         paintHorseCell(x, y, "selected_cell")
+
+        checkOptions(x, y)
+    }
+
+    private fun clearOptions() {
+        for (i in 0..7) {
+            for (j in 0..7) {
+                if (board[i][j] == 9 || board[i][j] == 2) {
+                    if (board[i][j] == 9 || board[i][j] == 0) {
+                        clearOption(i, j)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun clearOption(x: Int, y: Int) {
+
+        val iv: ImageView = findViewById(resources.getIdentifier("c$x$y", "id", packageName))
+
+        if (checkColorCell(x, y) == "black") {
+            iv.setBackgroundColor(
+                ContextCompat.getColor(
+                    this,
+                    resources.getIdentifier(nameColorBlack, "color", packageName)
+                )
+            )
+        } else {
+            iv.setBackgroundColor(
+                ContextCompat.getColor(
+                    this,
+                    resources.getIdentifier(nameColorWhite, "color", packageName)
+                )
+            )
+        }
+
+        if (board[x][y] == 1) iv.setBackgroundColor(
+            ContextCompat.getColor(
+                this,
+                resources.getIdentifier("previus_cell", "color", packageName)
+            )
+        )
+    }
+
+    private fun checkOptions(x: Int, y: Int) {
+        options = 0
+
+        checkMove(x, y, 1, 2)
+        checkMove(x, y, 2, 1)
+        checkMove(x, y, 1, -2)
+        checkMove(x, y, 2, -1)
+        checkMove(x, y, -1, 2)
+        checkMove(x, y, -2, 1)
+        checkMove(x, y, -1, -2)
+        checkMove(x, y, -2, -1)
+    }
+
+    private fun checkMove(x: Int, y: Int, movX: Int, movY: Int) {
+        val optionX = x + movX
+        val optionY = y + movY
+
+        if (optionX < 8 && optionY < 8 && optionX > 0 && optionY > 0) {
+            if (board[optionX][optionY] == 0 || board[optionX][optionY] == 2) {
+                options++
+                paintOptions(optionX, optionY)
+
+                board[optionX][optionY] = 9
+            }
+        }
+    }
+
+    private fun paintOptions(x: Int, y: Int) {
+        val iv: ImageView = findViewById(resources.getIdentifier("c$x$y", "id", packageName))
+        if (checkColorCell(x, y) == "black") iv.setBackgroundResource(R.drawable.option_black)
+        else iv.setBackgroundResource(R.drawable.option_white)
+    }
+
+    private fun checkColorCell(x: Int, y: Int): String {
+        var color = ""
+        val blackColumnX = arrayOf(0, 2, 4, 6)
+        val blackRowX = arrayOf(1, 3, 5, 7)
+
+        if ((blackColumnX.contains(x) && blackColumnX.contains(y)) || (blackRowX.contains(x) && blackRowX.contains(
+                y
+            ))
+        )
+            color = "black"
+        else color = "white"
+
+        return color
     }
 
     private fun paintHorseCell(x: Int, y: Int, color: String) {
@@ -77,7 +203,7 @@ class HorseGame : AppCompatActivity() {
         iv.setImageResource(R.drawable.hor)
     }
 
-    private fun iniScreenGame() {
+    private fun initScreenGame() {
         setSizeBoard()
         hideMessage()
     }
